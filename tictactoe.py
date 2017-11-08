59f96a613df95f6f13757df9f0ce196bc8796553
shepj2003 <shepj2003@gmail.com>
2017-11-08 11:44:00 +0000

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -1,14 +1,26 @@
-import numpy as np
+
 LMR = { 'L' : 0, 'M': 1, 'R' : 2}
 TMB = { 'T' : 0, 'M' : 1, 'B' : 2}
 PLAYER = { 1 : 'X', -1 : 'O', 0 : ' '}
-def init_grid( n ) : 
+
+def init_grid( n ) :
+    """
+    make a 3x3 grid with zeros in it
+    """
+    ## TODO 
+    ## this line will complain when you try to load it becuase 
+    ## numpy is a special library for doing maths that needs to be loaded into 
+    ## python.
+    ## do this by adding the line 
+    ## import numpy as np
+    ## this imports the library and calls it np ... just so don't have to keep typing numpy
     return np.zeros([n,n], dtype = int)
+
 def update_grid( grid, player, move) :
-    
-    ## if a valid move is played then update the grid and return 1
-    ## if the move is invalid then print out some help and return 0
-    
+    """
+    if a valid move is played then update the grid and return 1
+    if the move is invalid then print out some help and return 0
+    """
     if ( move[1] not in LMR.keys() )  or ( move[0] not in TMB.keys() ) :
         print ("moves consist of 2 characters")
         print ("first character should be one of T(op),M(iddle), B(ottom)")
@@ -18,10 +30,19 @@ def update_grid( grid, player, move) :
     y = LMR[ move[1] ]
     x = TMB[ move[0] ]
     if grid[x,y] == 0 :
-        grid[x,y] = player
+        ## TODO
+        ## this is a valid move so we need to update the grid
+        ## we have discovered that we need to update the point grid[x,y]
+        ## and we need to make it equal to player so we remember who went here
+        ## change the line below to update the value of grid[x,y] ..otherwise the game will never end
+        grid[x,y] = 0
         return 1 
+    
+    ## if we get here then it means that it was not a valid move 
+    ## so we ask the player to have another go
     print ( 'that space is already occupied. try again' )  
-    return 0 
+    return 0
+
 def check_lines( grid) : 
     l = len(grid)
     r = range ( l ) 
@@ -33,9 +54,17 @@ def check_lines( grid) :
     return all_lines
 
 def print_grid( grid) : 
+    """
+    print out a human readable version of the board
+    loop over all the rows and for each row print the chars for each row 
+    followed by a new-line
+    then add a line of --- to separate rows
+    """
     gstr = ''
     for col in grid : 
         gstr += "|".join( [PLAYER[ x ]   for x in col ] )
+        ## TODO :: add a newline to the gstr to make each row appear on a separate line
+        ## new line is "\n"
         gstr +="\n"
         gstr += "-----\n"
     print (gstr)
@@ -63,7 +92,8 @@ def switch_curr_player(curr_player) :
         if curr_player is 1 then return -1
         if curr_player is -1 then return 1
     """
-    return -1*curr_player
+    ## TODO :: fix the return value below
+    return 0
 
 def play() : 
     grid = init_grid(3)
