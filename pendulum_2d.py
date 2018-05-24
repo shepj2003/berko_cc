96719033b3d2e35b5c0cb4e770231536e26b3308
shepj2003 <shepj2003@gmail.com>
2018-05-24 08:58:50 +0100

    add todos, trace route of each pendulum bob

--- pendulum_2d.py
+++ pendulum_2d.py
@@ -14,12 +14,19 @@ from matplotlib.colors import cnames
 ## need the line below tomake the animation work in jupyter
 %matplotlib nbagg
 
+## TODO 
+## investigate the effect of changing the lengths & weights of each part of the pendulum
+## investigate changing gravity
+
 G = 9.8  # acceleration due to gravity, in m/s^2
-L1 = 1.5  # length of pendulum 1 in m
-L2 = 0.5  # length of pendulum 2 in m
+L1 = 1.0  # length of pendulum 1 in m
+L2 = 1.0  # length of pendulum 2 in m
 M1 = 1.0  # mass of pendulum 1 in kg
 M2 = 1.0  # mass of pendulum 2 in kg
 
+## TODO 
+## change the colour scheme 
+## https://matplotlib.org/users/colormaps.html
 colors = plt.cm.jet(np.linspace(0, 1, 50))
 
 def derivs(state, t):
@@ -72,13 +79,24 @@ ax = fig.add_subplot(111, autoscale_on=False, xlim=(-2, 2), ylim=(-2, 2))
 ax.set_aspect('equal')
 ax.grid()
 
+## TODO 
+## instead of showing the pendulum .. trace the path of 
+## the ends of each arm of the pendulum
+## line_p1 is for the first arm, line_p2 is for the 2nd arm 
+
 line, = ax.plot([], [], 'o-', lw=2)
+#line_p2, = ax.plot([], [], '-', lw=1)
+#line_p1, = ax.plot([], [], '-', lw=1)
+
 time_template = 'time = %.1fs'
 time_text = ax.text(0.05, 0.9, '', transform=ax.transAxes)
 
 
 def init():
-    line.set_data([], [])
+#    line.set_data([], [])
+#    line_p1.set_data(], []))
+#    line_p2.set_data([], [])
+    
     time_text.set_text('')
     return line, time_text
 
@@ -86,13 +104,27 @@ def init():
 def animate(i):
     thisx = [0, x1[i], x2[i]]
     thisy = [0, y1[i], y2[i]]
+    
+    p2_x = [x2[:i]]
+    p2_y = [y2[:i]]
+    
+    p1_x = [x1[:i]]
+    p1_y = [y1[:i]]
+    
 
     line.set_data(thisx, thisy)
     line.set_color(colors[i%50])
+    
+#    line_p2.set_data(p2_x, p2_y)
+#    line_p2.set_color(colors[30])
+    
+#    line_p1.set_data(p1_x, p1_y)
+#    line_p1.set_color(colors[20])
+
+
     time_text.set_text(time_template % (i*dt))
     return line, time_text
 
 ani = animation.FuncAnimation(fig, animate, np.arange(1, len(y)),
-                              interval=100, blit=True, init_func=init)
+interval=200, blit=True, init_func=init)
 
-plt.show()
